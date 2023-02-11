16f3213417646ca57f0e0e8722b0bd2dc3c92db7
Asl161997 <karlitaherrera365@gmail.com>
2023-02-11 17:50:28 -0500

    Cambios tabla departamento

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(require('./routes/index'));
 
 //EJECUTION
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
     console.log(`Tu server esta listo por el puerto ${port}`)
