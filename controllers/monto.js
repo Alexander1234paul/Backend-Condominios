77851e5467d16eaef197ab38f1563f76f0e05240
bcastillo25 <brayan22ct@gmail.com>
2023-02-21 14:13:37 -0500

    cambios

--- controllers/monto.js
+++ controllers/monto.js
@@ -3,34 +3,39 @@ const { db } = require("../Conexiones/slq")
 
 const getAllMonto = (request, response) => {
 
-    db.query('select * from gest_adm_monto order by mon_id', (error, results) => {
-        if (error)
-            throw error
-        response.status(200).json(results.rows)
+    db.query(`select * from gest_adm_monto`, (error, results) => {
+        if (error) {
+            //throw error
+            response.status(201).sjson(results.rows)
+        } else {
+            //response.status(201).json(results.rows)
+        }
     })
 }
 
 const getByMonto = (request, response) => {
 
     const mon_id = request.params.mon_id;
-
-    console.log('id' + mon_id)
-    db.query('SELECT * FROM gest_adm_monto WHERE mon_id = $1', [mon_id], (error, results) => {
+    db.query(`select * from gest_adm_monto WHERE mon_id = $1`, [mon_id], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).json(results.rows[0])
         }
-        response.status(200).json(results.rows)
     })
 }
 
 const createMonto = (request, response) => {
     const { mon_precio, mon_fecha } = request.body
 
-    db.query('INSERT INTO gest_adm_monto (mon_precio, mon_fecha) VALUES ($1, $2)', [mon_precio, mon_fecha], (error, results) => {
+    db.query(`INSERT INTO gest_adm_monto (mon_precio, mon_fecha) VALUES ($1, $2)`, [mon_precio, mon_fecha], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
         }
-        response.status(201).send(`Cuota added with ID: ${mon_precio}`)
     })
 }
 
@@ -39,24 +44,26 @@ const updateMonto = (request, response) => {
     const { mon_precio, mon_fecha } = request.body
     console.log('id' + mon_id)
 
-    db.query('update gest_adm_monto set  mon_precio=$1, mon_fecha=$2 where mon_id=$3', [mon_precio, mon_fecha, mon_id], (error, results) => {
+    db.query(`update gest_adm_monto set  mon_precio=$1, mon_fecha=$2 where mon_id=$3`, [mon_precio, mon_fecha, mon_id], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
         }
-        response.status(200).send(`Cuota modified with ${mon_id}`)
     })
 }
 
 const deleteMonto = (request, response) => {
 
     const mon_id = request.params.mon_id;
-
-    //console.log('id' + mon_id)
-
-    db.query('delete from gest_adm_monto where mon_id=$1', [mon_id], (error, results) => {
-        if (error)
-            throw error
-        response.status(200).send(`Delete id ${mon_id}`)
+    db.query(`delete from gest_adm_monto where mon_id=$1`, [mon_id], (error, results) => {
+        if (error) {
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
+        }
     })
 }
 
--- controllers/multa.js
+++ controllers/multa.js
@@ -4,9 +4,12 @@ const { db } = require("../Conexiones/slq")
 const getAllMulta = (request, response) => {
 
     db.query('SELECT (pe.per_apellidos, pe.per_nombres) as residente, ml.mul_descripcion, mo.mon_precio, ml.mul_fecha, ml.mul_estado  FROM cont_multa ml, gest_adm_monto mo, seg_sis_residente r, seg_sis_persona pe where ml.mon_id = mo.mon_id and ml.mul_estado = false and ml.res_id = r.res_id and r.per_id = pe.per_id order by ml.mul_id', (error, results) => {
-        if (error)
-            throw error
-        response.status(200).json(results.rows)
+        if (error) {
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).json(results.rows)
+        }
     })
 }
 
@@ -17,9 +20,11 @@ const getByMulta = (request, response) => {
     console.log('id' + mul_id)
     db.query('SELECT * FROM cont_multa WHERE mul_id = $1', [mul_id], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).json(results.rows[0])
         }
-        response.status(200).json(results.rows)
     })
 }
 
@@ -28,9 +33,11 @@ const createMulta = (request, response) => {
 
     db.query('INSERT INTO cont_multa (mon_id, res_id, mul_estado, mul_fecha, mul_descripcion, mul_total) VALUES ($1, $2, $3, $4, $5, $6)', [mon_id, res_id, mul_estado, mul_fecha, mul_descripcion, mul_total], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
         }
-        response.status(201).send(`Multa added with ID: ${res_id}`)
     })
 }
 
@@ -41,9 +48,11 @@ const updateMulta = (request, response) => {
 
     db.query('update cont_multa set mon_id=$2, res_id=$3, mul_estado=$4, mul_fecha=$5, mul_descripcion=$6, mul_total=$7 where mul_id=$1', [mon_id, res_id, mul_estado, mul_fecha, mul_descripcion, mul_total, mul_id], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
         }
-        response.status(200).send(`Multa modified with ${mul_id}`)
     })
 }
 
@@ -54,9 +63,11 @@ const pagarMulta = (request, response) => {
 
     db.query('update cont_multa set mul_estado=true, mul_total=0 where mul_id=$1', [mul_estado, mul_total], (error, results) => {
         if (error) {
-            throw error
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
         }
-        response.status(200).send(`Multa modified with ${mul_id}`)
     })
 }
 
@@ -64,12 +75,13 @@ const deleteMulta = (request, response) => {
 
     const mul_id = request.params.mul_id;
 
-    //console.log('id' + mul_id)
-
     db.query('delete from cont_multa where mul_id=$1', [mul_id], (error, results) => {
-        if (error)
-            throw error
-        response.status(200).send(`Delete id is ${mul_id}`)
+        if (error) {
+            //throw error
+            response.status(400).send(`{}`)
+        } else {
+            response.status(201).send(`{}`)
+        }
     })
 }
 
