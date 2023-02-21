2518c0df598da4f94951c8e6c48f35b05f21374f
Quinatoa <paul1234alexander@gmail.com>
2023-02-20 22:23:10 -0500

    Realizar reservaciones desde usuarios

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const router = Router()
 
-const { loginCtrl, sendEmail } = require("../controllers/auth")
+const { loginCtrl, sendEmail,sendEmailtoAll } = require("../controllers/auth")
 
 router.post('/auth', loginCtrl)
 router.post('/sendmail', sendEmail)
 
-
+router.post('/sendmailtoAll', sendEmailtoAll)
 
 module.exports = router
